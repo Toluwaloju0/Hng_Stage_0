31922df395a3a6313b4bd91e7f9bc2e79ecf7fdb
Toluwaloju0 <teecoded1999@gmail.com>
2025-01-29 19:40:24 +0100

    Hng task stage 0

---
+++ README.md
@@ -0,0 +1,7 @@
+An API to return the email used in HNG Tech, Date, time and GitHub repository url
+# Installation
+# Usage
+
+# Installation
+- Run the command ./setup to set the server for deployment
+- 
---
+++ api/hng_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""A module to get the hng stage 0 task cleared"""
+
+from flask import Flask, jsonify
+from datetime import datetime
+
+App = Flask(__name__)
+
+@App.route('/')
+def home():
+    return "Hello world"
+
+@App.route('/Hng_task_0', strict_slashes=False)
+def stage_0():
+    cur_date = datetime.now().isoformat()
+    return jsonify({
+        "email": "tolukayode2017@gmail.com",
+        "current_datetime": cur_date,
+        "github_url": "https://github.com/Toluwaloju0/Hng_Stage_0"
+    })
+
+if __name__ == '__main__':
+    App.run(host='0.0.0.0', port=4000)
